44f64e7ed1dadb4802488f2826ea13d5bf601aa8
amarglon <31377395+amarglon@users.noreply.github.com>
2017-09-26 20:05:28 -0700

    defined multiply function

--- polynomial.h
+++ polynomial.h
@@ -24,12 +24,19 @@ T Polynomial<T>::add(T coefOne, T coefTwo, T coefThree){
     a += coefOne;
     b += coefTwo;
     c += coefThree;
-    std::cout<< "The new Polynomial is: "<< endl;
+    std::cout<< "The new polynomial is: "<< endl;
     std::cout<< a <<"x^2 + " << b << "x + " << c << endl;
 }
 template <class T>
 T Polynomial<T>::multiply(T coefOne, T coefTwo, T coefThree){
-//Some code here
+    std::cout << "multiplying polynomials..." << endl;
+    T degreeFour = a*coefOne;
+    T degreeThree = (a*coefTwo)+(b*coefOne);
+    T degreeTwo = (a*coefThree) + (b*coefTwo) + (c*coefOne);
+    T degreeOne = (b*coefThree) + (c*coefTwo);
+    T coef = (c*coefThree);
+    std::cout << "The new polynomial is: " << endl;
+    std::cout << degreeFour << "x^4 + " << degreeThree << "x^3 + " << degreeTwo << "x^2 + " << degreeOne << "x + " << coef << endl;
 }
 template <class T>
 T Polynomial<T>::getA(){
